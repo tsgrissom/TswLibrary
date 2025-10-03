77dbd75394889cc9d8757f313269367a1bd4ce1a
Tyler Grissom <tylergrissom@me.com>
2025-10-02 20:51:25 -0500

    Convenience

--- Sources/TxSwift/SwiftData/ModelContainer.swift
+++ Sources/TxSwift/SwiftData/ModelContainer.swift
@@ -5,13 +5,22 @@
 import SwiftData
 
 public extension ModelContainer {
-	convenience init(for schema: Schema, cfg configurations: [ModelConfiguration]) {
-		try! self.init(for: schema, configurations: configurations)
-	}
-	convenience init (_ schema: Schema, cfg configurations: [ModelConfiguration]) {
-		try! self.init(for: schema, configurations: configurations)
-	}
-	convenience init(_ schema: Schema, cfg configurations: ModelConfiguration...) {
-		try! self.init(for: schema, configurations: configurations)
+//	convenience init(for schema: Schema, cfg configurations: [ModelConfiguration]) {
+//		try! self.init(for: schema, configurations: configurations)
+//	}
+//	convenience init (_ schema: Schema, cfg configurations: [ModelConfiguration]) {
+//		try! self.init(for: schema, configurations: configurations)
+//	}
+//	convenience init(_ schema: Schema, cfg configurations: ModelConfiguration...) {
+//		try! self.init(for: schema, configurations: configurations)
+//	}
+	convenience init(_ schema: Schema, _ config: ModelConfiguration) throws {
+		try self.init(for: schema, configurations: [config])
 	}
 }
+//
+//public convenience init(for forTypes: any PersistentModel.Type..., migrationPlan: (any SchemaMigrationPlan.Type)? = nil, configurations: ModelConfiguration...) throws
+//
+//public convenience init(for givenSchema: Schema, migrationPlan: (any SchemaMigrationPlan.Type)? = nil, configurations: ModelConfiguration...) throws
+//
+//public init(for givenSchema: Schema, migrationPlan: (any SchemaMigrationPlan.Type)? = nil, configurations: [ModelConfiguration]) throws
